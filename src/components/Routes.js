214f677cebce6fc7038bd4922d0c38ca885ad5b7
Alex Howard <thezanke@gmail.com>
2017-10-21 16:16:02 -0400

    Adding code splitting for main pages

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import asyncComponent from './AsyncComponent.hoc';
+
+const AsyncAdmin = asyncComponent(() => import('./Admin.page.js'));
+const AsyncHome = asyncComponent(() => import('./Home.page.js'));
+const AsyncNotFound = asyncComponent(() => import('./NotFound.page.js'));
+
 const Routes = () => (
   <Switch>
-    <Route path="/" render={() => <div>Hello world</div>} />
+    <Route path="/admin" exact component={AsyncAdmin} />
+    <Route path="/" exact component={AsyncHome} />
+    <Route component={AsyncNotFound} />
   </Switch>
 );
 
